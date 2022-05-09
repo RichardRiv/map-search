391582bb9375e7219f6f1f732f280b983a0d5674
Richard Rivera <rivera.richard92@yahoo.com>
2022-05-09 12:54:21 -0400

    Modified Forms

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,58 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
-
-const GOOGLE_API_KEY = 'AIzaSyAoxIPsrmzVltncjD-unHrTxVgImTJXxL4';
 
 declare var google: any;
 
-type GoogleGeocodingResponse = {
-	results: { geometry: { location: { lat: number; lng: number } } }[];
-	status: 'OK' | 'ZERO_RESULTS';
-};
-
 const Forms = () => {
+	const form = document.querySelector('form')!;
+	const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-    const searchAddressHandler = async (e: Event) => {
-        e.preventDefault();
-        const enteredAddress = addressInput.value;
-    
-        try {
-            const res = await axios.get<GoogleGeocodingResponse>(
-                `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-                    enteredAddress
-                )}&key=${GOOGLE_API_KEY}`
-            );
-    
-            if (res.data.status !== 'OK') {
-                throw new Error('Could not fetch location!');
-            }
-    
-            const coordinates = res.data.results[0].geometry.location;
-            const map = new google.maps.Map(document.getElementById('map'), {
-                center: coordinates,
-                zoom: 16,
-            });
+	const GOOGLE_API_KEY = 'AIzaSyAoxIPsrmzVltncjD-unHrTxVgImTJXxL4';
 
-            new google.maps.Marker({ position: coordinates, map: map });
-        } catch (err: any) {
-        alert(err.message)
-            console.log(err);
-        }
-    };
+	type GoogleGeocodingResponse = {
+		results: { geometry: { location: { lat: number; lng: number } } }[];
+		status: 'OK' | 'ZERO_RESULTS';
+	};
 
-    form.addEventListener('submit', searchAddressHandler);
+	const searchAddressHandler = async (e: Event) => {
+		e.preventDefault();
+		const enteredAddress = addressInput.value;
 
-  return(
-      <div>
-          <form className='form-group'>
-              <input type="text" id='address' placeholder='Search Address'/>
-              <button type='submit'>Search!</button>
-          </form>
-      </div>
-  )
-}
+		try {
+			const res = await axios.get<GoogleGeocodingResponse>(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+					enteredAddress
+				)}&key=${GOOGLE_API_KEY}`
+			);
+
+			if (res.data.status !== 'OK') {
+				throw new Error('Could not fetch location!');
+			}
+
+			const coordinates = res.data.results[0].geometry.location;
+			const map = new google.maps.Map(document.getElementById('map'), {
+				center: coordinates,
+				zoom: 16,
+			});
+
+			new google.maps.Marker({ position: coordinates, map: map });
+		} catch (err: any) {
+			alert(err.message);
+			console.log(err);
+		}
+	};
+
+	form.addEventListener('submit', searchAddressHandler);
+
+	return (
+		<div>
+			<form className="form-group">
+				<input type="text" id="address" placeholder="Search Address" />
+				<button type="submit">Search!</button>
+			</form>
+		</div>
+	);
+};
 
 export default Forms;
